06d36d912ffc92a661ddfcd54fa8c0406dc18dbb
Drew Keller <drewkeller@uchicago.edu>
2022-01-31 22:40:20 -0600

    set up docker, fixed fcc again, improved api error catching

--- data_pipeline/fetch_fcc_data.py
+++ data_pipeline/fetch_fcc_data.py
@@ -14,6 +14,8 @@ FCC_URL = "https://opendata.fcc.gov/resource/hicn-aujz.json"
 def fcc_fixed(force_api_call=False):
     """Returns dataframe of Dec 2020 fixed broadband data from Form 477.
 
+    WARNING: Slow (due to API response) and returns 3.4M data points.
+
     Args:
     force_api_call (optional): When True, calls relevant API and writes local CSV.
         Otherwise, will preferentially just read local CSV. Defaults to False.
@@ -28,19 +30,27 @@ def fcc_fixed(force_api_call=False):
     csv_address = os.path.join(os.path.dirname(__file__), '../data/fcc_fixed.csv')
     while True:
         if force_api_call:
-            request = FCC_URL + "?stateabbr=IL&$$app_token=" + config['API Keys']['FCCAppToken']
-            response = requests.get(request)
-            try:
-                response = response.json()
-            except:
-                print(response)
-            data = pd.DataFrame(columns=response[0], data=response[1:])
-            data.to_csv(csv_address)   
-            return data 
+            data_list = []
+            offset = 0
+            while True:  # have to "page" through the FCC data
+                geography_url = "?stateabbr=IL"
+                # 50,000 is the max limit per page; order is needed to ensure pages do not contain duplicate data
+                paging_url = f"&$order=logrecno&$limit=50000&$offset={offset}"
+                token_url = "&$$app_token=" + config['API Keys']['FCCAppToken']
+                request = FCC_URL + geography_url + paging_url + token_url
+                response = requests.get(request)
+                try:
+                    response = response.json()
+                    data_list.append(pd.DataFrame(columns=response[0], data=response[1:]))
+                except:
+                    print(response)
+                offset += 49999  # advances to the next page
+                if data_list[-1].shape[0]<49999:  # once we don't get a full response, we've reached the last page
+                    data = pd.concat(data_list)
+                    data.to_csv(csv_address)   
+                    return data
         try:
             data = pd.read_csv(csv_address, index_col = 0)
             return data
         except:
             force_api_call = True
-
-fcc_fixed(force_api_call=True)
